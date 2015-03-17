b58f1cac866a6e2f3d1e8363260135c604d34477
Jared Barboza <jared.m.barboza@gmail.com>
2015-03-17 14:45:36 -0400

    cleanup, store files in .part while transferring, then mv to final location

--- app.js
+++ app.js
@@ -18,15 +18,6 @@ var markers = {
 net.createServer(function(sock) {
   var incomingFileName = null;
 
-  var peekBuffer = function(buffer, offset, end) {
-    offset = offset || 0;
-    end = end || (end === -1 ? undefined : end);
-
-    console.log('in peek: end = ' + end, 'offset = ' + offset);
-
-    return new Buffer(buffer.slice(offset, end));
-  };
-
   var processInstruction = function(instruction) {
     var isNewFile = instruction.txt.indexOf('newFile:') != -1;
     var isEndOfFile = instruction.txt.indexOf('EOF') != -1;
@@ -34,13 +25,23 @@ net.createServer(function(sock) {
     var isDataSegment = instruction.txt === 'data:segment';
 
     if(isNewFile) {
-      incomingFileName = instruction.txt.split(':')[1];
+      incomingFileName = instruction.txt.split(':')[1] + '.part';
       console.log('new file: ', incomingFileName);
     } else if(isEcho) {
       var txt = instruction.txt.split(':')[1];
       console.log('ECHO: ' + txt);
     } else if(isEndOfFile) {
       console.log('end of file: ', incomingFileName);
+      var finalPath = path.resolve(incomingFilePath, incomingFileName.replace('.part', ''));
+      var partialPath = path.resolve(incomingFilePath, incomingFileName);
+      var rename = function() { fs.rename(partialPath, finalPath); };
+      fs.exists(finalPath, function(exists) {
+        if(exists) {
+          fs.unlink(finalPath, rename);
+          return;
+        }
+        rename();
+      });
       incomingFileName = null;
     } else if(isDataSegment && incomingFileName != null) {
       var filePath = path.resolve(incomingFilePath, incomingFileName);
@@ -75,14 +76,12 @@ net.createServer(function(sock) {
       } else {
         // data segment?
         if(start === -1) start = buffer.length;
-        // console.log('would slice buffer: ', pos, 'to', start, 'len', buffer.length);
         processInstruction({
           txt: 'data:segment',
           data: buffer.slice(pos, start)
         });
         pos = start;
       }
-      // processInstruction(instruction);
     }
     console.log('-=-=-=-=-=-=-=- done processing -=-=-=-=-=-=-=-\n');
   });
